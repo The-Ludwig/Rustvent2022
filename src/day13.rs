21a9b40b0fd94097b2bd3b02f48ab9f1b9eb73fb
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2022-12-13 22:11:19 +0100

    🎅 Day 13

--- src/day13.rs
+++ src/day13.rs
@@ -1,32 +1,34 @@
 use rustvent2022::get_input;
+use std::cmp::Ordering;
 use std::error::Error;
-use std::fmt;
+// use std::fmt;
+use std::collections::BinaryHeap;
 use std::str::FromStr;
 
-#[derive(Debug)]
-struct StringError {
-    err: String,
-}
+// #[derive(Debug)]
+// struct StringError {
+//     err: String,
+// }
 
-impl StringError {
-    fn from_str(s: &str) -> Self {
-        (StringError { err: s.to_string() })
-    }
+// impl StringError {
+//     fn from_str(s: &str) -> Self {
+//         (StringError { err: s.to_string() })
+//     }
 
-    fn new(s: String) -> Self {
-        StringError { err: s }
-    }
-}
+//     fn new(s: String) -> Self {
+//         StringError { err: s }
+//     }
+// }
 
-impl fmt::Display for StringError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.err)
-    }
-}
+// impl fmt::Display for StringError {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{}", self.err)
+//     }
+// }
 
-impl Error for StringError {}
+// impl Error for StringError {}
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Eq, Ord, Debug)]
 enum RecList {
     List(Vec<RecList>),
     Num(usize),
@@ -44,9 +46,12 @@ impl FromStr for RecList {
                     depth += 1;
                 } else if c == ']' {
                     depth -= 1;
-                } else if depth == 0 && c == ',' {
-                    v.push(RecList::from_str(&s[start..i])?);
-                    start = i + 1;
+                }
+                if (depth == 0 && c == ',') || (depth == -1 && c == ']') {
+                    if start != i {
+                        v.push(RecList::from_str(&s[start..i])?);
+                        start = i + 1;
+                    }
                 }
             }
             return Ok(RecList::List(v));
@@ -56,26 +61,27 @@ impl FromStr for RecList {
     }
 }
 
-impl RecList{
-    fn less(&self, other: &RecList) -> Option(bool){
+impl PartialOrd for RecList {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match self {
-            RecList::List(l) => {
-                match other {
-                    
+            RecList::List(l) => match other {
+                RecList::List(lr) => {
+                    for i in 0..std::cmp::min(l.len(), lr.len()) {
+                        let res = l[i].partial_cmp(&lr[i]);
+                        if res != Some(Ordering::Equal) {
+                            return res;
+                        }
+                    }
+                    l.len().partial_cmp(&lr.len())
                 }
+                RecList::Num(n) => self.partial_cmp(&RecList::List(vec![RecList::Num(*n)])),
             },
-            RecList::Num(n) => {
-                match other {
-                    RecList::List(l) {
-                    
-                    },
-                    RecList::Num()
-                }
-            }
+            RecList::Num(n) => match other {
+                RecList::List(_) => RecList::List(vec![RecList::Num(*n)]).partial_cmp(other),
+                RecList::Num(nr) => n.partial_cmp(nr),
+            },
         }
-
     }
-
 }
 
 fn parse(input: &str) -> Vec<(RecList, RecList)> {
@@ -91,9 +97,36 @@ fn parse(input: &str) -> Vec<(RecList, RecList)> {
         .collect()
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    // let input = parse(&get_input("2022", "13"));
+fn part_one(input: &Vec<(RecList, RecList)>) -> usize {
+    let mut sum = 0;
+    for (i, (l1, l2)) in input.iter().enumerate() {
+        if l1 <= l2 {
+            sum += i + 1;
+        }
+    }
+    sum
+}
 
+fn part_two(input: &Vec<(RecList, RecList)>) -> usize {
+    let mut heap = BinaryHeap::new();
+    for (l1, l2) in input.iter() {
+        heap.push(l1);
+        heap.push(l2);
+    }
+    let div1 = RecList::List(vec![RecList::List(vec![RecList::Num(2)])]);
+    let div2 = RecList::List(vec![RecList::List(vec![RecList::Num(6)])]);
+    heap.push(&div1);
+    heap.push(&div2);
+    let sorted = heap.into_sorted_vec();
+    let i1 = sorted.iter().position(|&x| *x == div1).unwrap() + 1;
+    let i2 = sorted.iter().position(|&x| *x == div2).unwrap() + 1;
+    i1 * i2
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let input = parse(&get_input("2022", "13"));
+    println!("Solution part one: {}", part_one(&input));
+    println!("Solution part two: {}", part_two(&input));
     Ok(())
 }
 
@@ -129,20 +162,19 @@ mod tests {
     fn test_parse() {
         let input = parse(TEST);
 
-        assert_eq!(input[2].0, RecList::from_str("[9]").unwrap());
+        assert_eq!(input[2].0, RecList::List(vec![RecList::Num(9)]));
+        assert_eq!(input[5].0, RecList::List(Vec::new()));
     }
 
-    //     #[test]
-    //     fn test_part_one() {
-    //         let input = parse(TEST);
+    #[test]
+    fn test_part_one() {
+        let input = parse(TEST);
+        assert_eq!(part_one(&input), 13);
+    }
 
-    //         assert_eq!(part_one(&input), 31);
-    //     }
-
-    //     #[test]
-    //     fn test_part_two() {
-    //         let input = parse(TEST);
-
-    //         assert_eq!(part_two(&input), 29);
-    //     }
+    #[test]
+    fn test_part_two() {
+        let input = parse(TEST);
+        assert_eq!(part_two(&input), 140);
+    }
 }
